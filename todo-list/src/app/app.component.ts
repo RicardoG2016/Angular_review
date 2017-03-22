2cc9d60322d0b44bc406d11bf11556986a7a1129
RicardoG2016 <gomez.r@live.com>
2017-03-22 11:48:38 -0500

    created home component.

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  directives: [HomeComponent]
 })
 export class AppComponent {
   title = 'app works!';
