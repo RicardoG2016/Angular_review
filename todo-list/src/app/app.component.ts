4a77c667a10685b7a758450447feea7a00a31bae
RicardoG2016 <gomez.r@live.com>
2017-03-22 12:14:55 -0500

    reviewed data binding.

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  directives: [HomeComponent]
 })
 export class AppComponent {
   title = 'app works!';
--- todo-list/src/app/home/home.component.ts
+++ todo-list/src/app/home/home.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  homeTitle = "Welcome to the homepage...";
+  myString = "binded data"
+  
+  // event binding ex
+  alertMe(){
+    alert('Event is binded');
+  }
+
+  // two way binding
+  car = {
+    model: "Honda",
+    color: "Black"
+  };
+
 
   constructor() { }
 
